efc9a39a2e8f8d4c4e066ce7d1eaaf9ad4091a36
Adam Chalkley <atc0005@users.noreply.github.com>
2020-04-04 07:12:17 -0500

    Add missing license header, package description

--- teams/teams.go
+++ teams/teams.go
@@ -1,3 +1,15 @@
+// Copyright 2019 Adam Chalkley
+//
+// https://github.com/atc0005/send2teams
+//
+// Licensed under the MIT License. See LICENSE file in the project root for
+// full license information.
+
+// Package teams is, in essence, a waiting list of features and changes that
+// have not yet been submitted for inclusion into the upstream
+// https://github.com/dasrick/go-teams-notify project. The functions provided
+// by this package extend or enhance existing functionality provided by the
+// `dasrick/go-teams-notify` package.
 package teams
 
 import (
