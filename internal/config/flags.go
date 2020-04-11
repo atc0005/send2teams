292d1f572ab99d8d56d5a62b47b3e30396c039d4
Adam Chalkley <atc0005@users.noreply.github.com>
2020-04-10 23:21:48 -0500

    Minor spacing tweak, add/update copyright year

--- internal/config/flags.go
+++ internal/config/flags.go
@@ -1,3 +1,10 @@
+// Copyright 2020 Adam Chalkley
+//
+// https://github.com/atc0005/send2teams
+//
+// Licensed under the MIT License. See LICENSE file in the project root for
+// full license information.
+
 package config
 
 import "flag"
