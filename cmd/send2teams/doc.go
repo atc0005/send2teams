b43846c3e40c4c41de27aea1956796ba22de7d8f
Adam Chalkley <atc0005@users.noreply.github.com>
2022-09-02 04:58:47 -0500

    Add missing cmd doc file
    Add "package" doc file to resolve revive package-comment linting error
    surfaced by recent linter update.

---
+++ cmd/send2teams/doc.go
@@ -0,0 +1,20 @@
+// Copyright 2021 Adam Chalkley
+//
+// https://github.com/atc0005/send2teams
+//
+// Licensed under the MIT License. See LICENSE file in the project root for
+// full license information.
+
+// Small CLI tool used to submit messages to Microsoft Teams. send2teams is
+// intended for use by Nagios, scripts or other actions that may need to
+// submit pass/fail results to a MS Teams channel.
+//
+// See our [GitHub repo]:
+//
+//   - to review documentation (including examples)
+//   - for the latest code
+//   - to file an issue or submit improvements for review and potential
+//     inclusion into the project
+//
+// [GitHub repo]: https://github.com/atc0005/send2teams
+package main
